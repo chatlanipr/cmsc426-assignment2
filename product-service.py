b304ea97e8c3c446d25bcc7d658a70daff9fee2c
chatlanipr <chatlanipr@vcu.edu>
2024-09-22 18:42:11 -0400

    edits

--- product-service.py
+++ product-service.py
@@ -2,15 +2,12 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-
 product_list = [
     {'id': 1, 'name': 'Donuts', 'price': 5, 'quantity': 30},
     {'id': 2, 'name': 'Bananas', 'price': 0.45, 'quantity': 50},
     {'id': 3, 'name': 'Bread', 'price': 1.50, 'quantity': 100},
     {'id': 4, 'name': 'Fish', 'price': 8, 'quantity': 10},
 ]
-
-product_id = len(product_list) #current product_id
 
 # /products (GET): Retrieve a list of available grocery products, including their names, prices, and quantities in stock.
 @app.route('/products', methods=['GET'])
